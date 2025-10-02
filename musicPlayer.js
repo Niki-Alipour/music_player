844507f5da0c33839dbffb507e698f91e0bcb06d
niki <nikiali004@gmail.com>
2025-10-02 12:22:43 +0330

    all the accesse and the cover set

---
+++ musicPlayer.js
@@ -0,0 +1,38 @@
+musics = [ // your music itemes 
+    { name  : "Skyfall" ,
+      cover : "cover/skyfall.jpg",
+      audio : new Audio ("./audio/skyfall.mp3")
+
+    }
+]
+// Accesses
+musicName = document.querySelector("#music-name");
+range =  document.querySelector("#music-time");
+musicCover = document.querySelector("#music-cover");
+preBtn = document.querySelector("#pre-btn");
+nextBtn = document.querySelector("#next-btn");
+playBtn = document.querySelector("#play-btn");
+
+currentMusic = 0;
+let audio = musics[currentMusic].audio
+
+musicCover.src = musics[currentMusic].cover
+
+musicName.innerText = musics[currentMusic].name
+
+audio.addEventListener("canplay",()=>{
+  range.max = audio.duration
+
+})
+
+audio.addEventListener("timeupdate",()=>{
+  range.value = audio.currentTime
+
+} )
+
+range.addEventListener("input", ()=>{
+  audio.currentTime = range.value
+
+})
+
+
